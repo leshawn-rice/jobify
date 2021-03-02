4ecffcf0da25b7c7ffc23be9bc1599a3ac0c690e
leshawn-rice <leshawnrice3@gmail.com>
2021-03-01 21:27:42 -0800

    made content div wider, refactored search (linkedin is rate limiting)

--- helpers/jobSearch.js
+++ helpers/jobSearch.js
@@ -4,25 +4,33 @@ const jsdom = require('jsdom');
 const $ = require('jquery')(new jsdom.JSDOM().window);
 
 async function findDescription(link) {
-  const res = await axios.get(link);
+  const description = [];
 
-  const page = $(res.data);
+  const res = await axios.get(link)
+    .catch(err => {
+      console.error(err);
+    });
 
-  const raw = page.find('div.show-more-less-html__markup.show-more-less-html__markup--clamp-after-5');
+  try {
+    const page = $(res.data);
+    const raw = page.find('div.show-more-less-html__markup.show-more-less-html__markup--clamp-after-5');
 
-  const description = raw.text().trim();
-  return description;
+    for (let child of raw.children()) {
+      description.push($(child).text());
+    }
+
+    return description;
+  }
+  catch (err) {
+    return [];
+  }
 }
 
 async function parseJobCard(card) {
-
-  // Need to get a description
-
   const title = card.find('h3.result-card__title.job-result-card__title').text();
   const company = card.find('a.result-card__subtitle-link.job-result-card__subtitle-link').text();
   const location = card.find('span.job-result-card__location').text();
   const link = card.find('a.result-card__full-card-link').attr('href');
-
   const description = await findDescription(link);
 
   return {
@@ -32,34 +40,33 @@ async function parseJobCard(card) {
 
 async function parseHTML(response) {
   const pageJobs = []
-
   const html = $(response.data);
   const jobCards = html.find('.result-card');
 
   for (let card of jobCards) {
-    const job = await parseJobCard($(card));
+    const job = parseJobCard($(card));
     pageJobs.push(job);
   }
-  return pageJobs;
+  return await Promise.all(pageJobs);
 }
 
 async function findJobs(term, city, stateCode, exp, numResults) {
   const state = states.find(state => state.code === stateCode).name;
   const BASE_URL = 'https://www.linkedin.com/jobs/search'
   const jobs = []
+  const pages = []
 
-  let start = 0;
-
-  while (start < numResults) {
+  for (let start = 0; start < numResults; start += 25) {
     const url = `${BASE_URL}/?f_E=${exp}&keywords=${term}&location=${city} ${state} United States&start=${start}`
     const response = await axios.get(url);
-
-    const pageOfJobs = await parseHTML(response);
-
-    jobs.push(...pageOfJobs);
-
-    start += 25;
+    pages.push(parseHTML(response));
   }
+  const resolved = [...await Promise.all(pages)];
+
+  for (let page of resolved) {
+    jobs.push(...page);
+  }
+
   return jobs
 }
 
