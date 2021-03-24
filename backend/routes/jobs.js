9f22071212744f6f0c1ba5af58315f2f0282a5b2
leshawn-rice <leshawnrice3@gmail.com>
2021-03-23 23:35:31 -0700

    removed public file from backend

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -8,10 +8,6 @@ const express = require("express");
 const { BadRequestError } = require("../expressError");
 const { ensureAdmin } = require("../middleware/auth");
 const Job = require("../models/job");
-const jobNewSchema = require("../schemas/jobNew.json");
-const jobUpdateSchema = require("../schemas/jobUpdate.json");
-const jobSearchSchema = require("../schemas/jobSearch.json");
-
 const router = express.Router({ mergeParams: true });
 
 
@@ -24,20 +20,20 @@ const router = express.Router({ mergeParams: true });
  * Authorization required: admin
  */
 
-router.post("/", ensureAdmin, async function (req, res, next) {
-  try {
-    const validator = jsonschema.validate(req.body, jobNewSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
-    }
+// router.post("/", ensureAdmin, async function (req, res, next) {
+//   try {
+//     const validator = jsonschema.validate(req.body, jobNewSchema);
+//     if (!validator.valid) {
+//       const errs = validator.errors.map(e => e.stack);
+//       throw new BadRequestError(errs);
+//     }
 
-    const job = await Job.create(req.body);
-    return res.status(201).json({ job });
-  } catch (err) {
-    return next(err);
-  }
-});
+//     const job = await Job.create(req.body);
+//     return res.status(201).json({ job });
+//   } catch (err) {
+//     return next(err);
+//   }
+// });
 
 /** GET / =>
  *   { jobs: [ { id, title, salary, equity, companyHandle, companyName }, ...] }
@@ -97,20 +93,20 @@ router.get("/:id", async function (req, res, next) {
  * Authorization required: admin
  */
 
-router.patch("/:id", ensureAdmin, async function (req, res, next) {
-  try {
-    const validator = jsonschema.validate(req.body, jobUpdateSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
-    }
+// router.patch("/:id", ensureAdmin, async function (req, res, next) {
+//   try {
+//     const validator = jsonschema.validate(req.body, jobUpdateSchema);
+//     if (!validator.valid) {
+//       const errs = validator.errors.map(e => e.stack);
+//       throw new BadRequestError(errs);
+//     }
 
-    const job = await Job.update(req.params.id, req.body);
-    return res.json({ job });
-  } catch (err) {
-    return next(err);
-  }
-});
+//     const job = await Job.update(req.params.id, req.body);
+//     return res.json({ job });
+//   } catch (err) {
+//     return next(err);
+//   }
+// });
 
 /** DELETE /[handle]  =>  { deleted: id }
  *
